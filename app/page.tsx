0127643dd30257f339d3f945c43d23cf94c39ef3
Ariel Ivan Mariani <ariel@interactivelabs.co>
2022-12-20 11:01:42 -0300

    Updates favicon and head title

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-import Input from "../src/components/Input";
-import { FormEvent } from "react";
 import Form from "../src/components/Form";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,11 +12,12 @@ interface FormData {
 }
 
 export default function Home() {
-
   return (
     <main className="bg-gray-100 min-h-screen py-2">
-        <h1 className="text-3xl text-center font-bold text-indigo-500">Baulcito Contacts Form</h1>
-        <Form />
+      <h1 className="text-3xl text-center font-bold text-indigo-500">
+        Baulcito Contacts Form
+      </h1>
+      <Form />
     </main>
   );
 }
